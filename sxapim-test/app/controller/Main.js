5c72bb9db69a920cf8d9a7365d89ff9ceecb10d1
startxfr <>
2013-12-02 22:09:49 +0100

    suppression du test

--- sxapim-test/app/controller/Main.js
+++
@@ -1,3 +0,0 @@
-Ext.define('sxapim.controller.Main', {
-    extend: 'Ext.app.Controller'
-});
--- sxapim/build/temp/production/sxapim/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\Users\chris\Documents\NetBeansProjects\sxapi-m\sxapim\app.js
