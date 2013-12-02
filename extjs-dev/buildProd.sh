1be081b32c8b5dfce7d0e5d01ce39d015fef7211
startxfr <>
2013-12-02 15:51:35 +0100

    suppression de la maquette faite il y a 3mois

--- extjs-dev/buildProd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /secure/github/sxapi-m/extjs-dev
-sencha app build
-cp -R assets build/SxapiManager/production/
--- extjs-dev/buildTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /secure/github/sxapi-m/extjs-dev
-sencha app build testing
-cp -R assets build/SxapiManager/testing/
