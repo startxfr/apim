69bab0333b6753c06d98385ec6a55cd9bacb622c
startxfr <>
2013-12-03 20:55:43 +0100

    mise en place de la version de production

--- bootstrap.js
+++ bootstrap.js
@@ -412,7 +412,6 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1278,7 +1277,6 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
--- build/production/sxapim/resources/Readme.md
+++ build/production/sxapim/resources/Readme.md
@@ -1,3 +1,3 @@
-# ext-theme-neptune/resources
+# ext-theme-gray/resources
 
 This folder contains static resources (typically an `"images"` folder as well).
